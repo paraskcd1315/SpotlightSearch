f81c0af761a9d33192017134151b98dfbe199e33
Paras Khanchandani <dev@paraskcd.com>
2025-08-29 19:04:37 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.paraskcd.spotlightsearch"
         minSdk = 34
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.2.3"
+        versionCode = 16
+        versionName = "1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
